cde888e04e72efe606f25879e4293c63cdbd52be
Max Holman <max@holmn.com>
2023-05-11 16:11:15 +0800

    feat: support for empty content type in response (ie void response)

--- lib/request-body.ts
+++ lib/request-body.ts
@@ -3,7 +3,7 @@ import type { OpenAPIV3_1 } from 'openapi-types';
 import { MediaType, type MediaTypeOptions } from './media-type.js';
 
 export interface RequestBodyOptions {
-  content: MediaType | MediaTypeOptions;
+  content?: MediaType | MediaTypeOptions;
   description?: string;
   required?: boolean;
 }
@@ -11,23 +11,27 @@ export interface RequestBodyOptions {
 export class RequestBody extends Construct {
   private options: RequestBodyOptions;
 
-  private content: MediaType;
+  private content: MediaType | undefined;
 
   constructor(scope: Construct, id: string, options: RequestBodyOptions) {
     super(scope, id);
     this.options = options;
 
-    this.content =
-      options.content instanceof MediaType
-        ? options.content
-        : new MediaType(this, id, options.content);
+    if (options.content) {
+      this.content =
+        options.content instanceof MediaType
+          ? options.content
+          : new MediaType(this, id, options.content);
+    }
   }
 
   public synth(): OpenAPIV3_1.RequestBodyObject {
     return {
       description: this.options.description || '',
       content: {
-        [this.options.content.contentType]: this.content.synth(),
+        ...(this.content && {
+          [this.content.contentType]: this.content.synth(),
+        }),
       },
       ...(this.options.required && { required: this.options.required }),
     };
